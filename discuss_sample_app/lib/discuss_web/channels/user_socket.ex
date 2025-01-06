eaa040c86bcda938794fda1fd5a2a3dc7584559c
Cyril ViXP <cyrilvixp@gmail.com>
2025-01-06 16:01:39 +0200

    Add socket and channel

---
+++ discuss_sample_app/lib/discuss_web/channels/user_socket.ex
@@ -0,0 +1,23 @@
+defmodule DiscussWeb.UserSocket do
+  use Phoenix.Socket
+
+  channel "comments:*", DiscussWeb.CommentsChannel
+
+  @impl true
+  def connect(_params, socket, _connect_info) do
+    {:ok, socket}
+  end
+
+  # Socket IDs are topics that allow you to identify all sockets for a given user:
+  #
+  #     def id(socket), do: "user_socket:#{socket.assigns.user_id}"
+  #
+  # Would allow you to broadcast a "disconnect" event and terminate
+  # all active sockets and channels for a given user:
+  #
+  #     Elixir.DiscussWeb.Endpoint.broadcast("user_socket:#{user.id}", "disconnect", %{})
+  #
+  # Returning `nil` makes this socket anonymous.
+  @impl true
+  def id(_socket), do: nil
+end
